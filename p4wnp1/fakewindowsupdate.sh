1f1e2ef28f573d304a81ceddbb53aa82727694e4
Merlijn <merlijnvermeer@gmail.com>
2020-02-20 17:23:17 +0100

    fakewindows update

---
+++ p4wnp1/fakewindowsupdate.sh
@@ -0,0 +1,7 @@
+layout('us');			// US keyboard layout
+typingSpeed(1,0)	// Wait 100ms between key strokes + an additional random value between 0ms and 150ms (natural)
+
+delay(200)
+press("GUI r");
+delay(500);
+type("iexplore -k http://fakeupdate.net/win10u/index.html\n")
